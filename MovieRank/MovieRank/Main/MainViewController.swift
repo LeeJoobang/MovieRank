defaa86a368413fe7290c52090461b9f174e9546
Joobang <hii5074@gmail.com>
2023-04-25 23:34:23 +0900

    [add] navigation large title 적용

---
+++ MovieRank/MovieRank/Main/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  MovieRank
+//
+//  Created by Joobang on 2023/04/25.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        self.title = "Movie Rank"
+    }
+}
